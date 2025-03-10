6770034b3838e498e77730852c2a98fe4b75334f
OtschoP <35844438+OtschoP@users.noreply.github.com>
2025-03-10 10:39:43 +0100

    first commit

---
+++ js/data.js
@@ -0,0 +1,77 @@
+let myDishes = [
+    {
+        "name": "Pizza Margherita",
+        "price": 6.50,
+        "description": "Leckere, vegetarische Pizza mit Käse und Tomatensauce"
+    },
+    {
+        "name": "Pizza Vegetaria",
+        "price": 7.50,
+        "description": "Vegetarische Pizza belegt mit Gemüse der Saison"
+    },
+    {
+        "name": "Pizza Salami",
+        "price": 8.00,
+        "description": "Klassische Pizza mit würziger Salami, Tomatensauce und Käse"
+    },
+    {
+        "name": "Pizza Quattro Formaggi",
+        "price": 8.50,
+        "description": "Pizza mit vier verschiedenen Käsesorten für Käseliebhaber"
+    },
+    {
+        "name": "Spaghetti Carbonara",
+        "price": 9.00,
+        "description": "Klassische Pasta mit Ei, Speck, Parmesan und Sahnesauce"
+    },
+    {
+        "name": "Penne Arrabbiata",
+        "price": 7.50,
+        "description": "Scharfe Pasta mit Tomatensauce, Chili und Knoblauch"
+    },
+    {
+        "name": "Lasagne al Forno",
+        "price": 9.50,
+        "description": "Überbackene Lasagne mit Hackfleischsauce, Béchamel und Käse"
+    },
+    {
+        "name": "Insalata Caprese",
+        "price": 6.00,
+        "description": "Tomaten-Mozzarella-Salat mit frischem Basilikum und Olivenöl"
+    },
+    {
+        "name": "Insalata Mista",
+        "price": 5.50,
+        "description": "Gemischter Salat mit verschiedenen frischen Zutaten der Saison"
+    },
+    {
+        "name": "Tiramisu",
+        "price": 5.00,
+        "description": "Klassisches italienisches Dessert mit Mascarpone, Kaffee und Kakao"
+    },
+    {
+        "name": "Panna Cotta",
+        "price": 4.50,
+        "description": "Italienische Vanillecreme mit fruchtiger Beerensoße"
+    },
+    {
+        "name": "Espresso",
+        "price": 2.00,
+        "description": "Kräftiger italienischer Kaffee"
+    },
+    {
+        "name": "Cappuccino",
+        "price": 3.00,
+        "description": "Espresso mit aufgeschäumter Milch"
+    },
+    {
+        "name": "Limoncello Spritz",
+        "price": 6.50,
+        "description": "Erfrischender Cocktail mit Limoncello, Prosecco und Soda"
+    },
+    {
+        "name": "Aqua Minerale",
+        "price": 2.50,
+        "description": "Italienisches Mineralwasser mit oder ohne Kohlensäure"
+    }
+];
